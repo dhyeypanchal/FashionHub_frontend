2b0475ae24908b65f5a3adedd2d1b4198b1fe938
dhyeypanchal <dhyey22403@gmail.com>
2023-06-03 21:42:52 +0530

    backend url of images updated

--- src/pages/Admin/Products.js
+++ src/pages/Admin/Products.js
@@ -41,7 +41,7 @@ const Products = () => {
               >
                 <div className="card m-2" style={{ width: "18rem" }}>
                   <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
+                    src={`https://fashionhub-backend.onrender.com/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                     height={300}
